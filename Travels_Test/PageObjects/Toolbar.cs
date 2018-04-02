c1684b6590933de44d4e22eb07e8cc6394bf12e1
SGlado <sergii.glado@gmail.com>
2018-04-02 12:59:06 +0300

    changes 1 (04.02.2018)

---
+++ Travels_Test/PageObjects/Toolbar.cs
@@ -0,0 +1,72 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace Travels_Test.PageObjects
+{
+    class AccountPage
+    {
+        protected IWebDriver Driver { get; set; }
+        public AccountPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public IWebElement AccountDropdown => Driver.FindElement(By.XPath("/ html / body / div[4] / div / div / div[2] / ul / li[1] / a"));
+        public IWebElement LogoutButton => Driver.FindElement(By.XPath("//div[@class='tbar-top hidden-sm hidden-xs']//*[text()='  Logout']"));
+
+
+        internal AccountPage Logout()
+        {
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(p => AccountDropdown.Displayed == true);
+            Thread.Sleep(3000);
+
+            AccountDropdown.Click();
+
+            wait.Until(p => LogoutButton.Displayed == true);
+            LogoutButton.Click();
+            Driver.Navigate().GoToUrl("https://www.phptravels.net/");
+            return new AccountPage(Driver);
+        }
+
+    }
+    class Currency
+    {
+        protected IWebDriver Driver { get; set; }
+        public Currency(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+        public IWebElement CurrencyDropdown => Driver.FindElement(By.XPath("//div[@class='tbar-top hidden-sm hidden-xs']//li[@id='li_myaccount']/following-sibling::li"));
+        internal Currency ChangeCurrency(string newCurrency)
+        {
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(p => CurrencyDropdown.Displayed == true);
+            CurrencyDropdown.Click();
+
+            Driver.FindElement(By.XPath(String.Format("//div[@class='tbar-top hidden-sm hidden-xs']//a[@onclick='change_currency(this)' and contains(text(), '{0}') ]", newCurrency))).Click();
+            return new Currency(Driver);
+        }
+    }
+    class Language
+    {
+        protected IWebDriver Driver { get; set; }
+        public Language(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+        public IWebElement LanduageDropdown => Driver.FindElement(By.XPath("//div[@class='tbar-top hidden-sm hidden-xs']//li[@id='li_myaccount']/following-sibling::li"));
+        internal Language ChangeLanguage(string newLanguage)
+        {
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(p => LanduageDropdown.Displayed == true);
+            LanduageDropdown.Click();
+
+            Driver.FindElement(By.XPath(String.Format("//div[@class='tbar-top hidden-sm hidden-xs']//a[@onclick='change_language(this)' and contains(text(), '{0}') ]", newLanguage))).Click();
+            return new Language(Driver);
+            
+        }
+    }
+}
--- Travels_Test/Tests/LanguageTests.cs
+++ Travels_Test/Tests/LanguageTests.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
 using Travels_Test.Framework;
 using Travels_Test.PageObjects;
 
@@ -8,42 +7,42 @@ namespace Travels_Test.Tests
     [TestClass]
     public class LanguageTests : BaseTest
     {
-  
+
         [TestMethod] //CHANGE LANGUAGE TO ARABIC
         public void LanguageArabic()
         {
             var Language = new Language(Driver);
-            var arabicLanguage = Language.Arabic();
+            Language.ChangeLanguage("ARABIC");
         }
         [TestMethod] //CHANGE LANGUAGE TO TURKISH
         public void LanguageTurkish()
         {
             var Language = new Language(Driver);
-            var turkishLanguage = Language.Turkish();
+            Language.ChangeLanguage("TURKISH");
         }
         [TestMethod] //CHANGE LANGUAGE TO FRENCH
         public void LanguageFrench()
         {
             var Language = new Language(Driver);
-            var frenchLanguage = Language.French();
+            Language.ChangeLanguage("FRENCH");
         }
         [TestMethod] //CHANGE LANGUAGE TO SPANISH
         public void LanguageSpanish()
         {
             var Language = new Language(Driver);
-            var spanishLanguage = Language.Spanish();
+            Language.ChangeLanguage("SPANISH");
         }
         [TestMethod] //CHANGE LANGUAGE TO RUSSIAN
         public void LanguageRussian()
         {
             var Language = new Language(Driver);
-            var russianLanguage = Language.Russian();
+            Language.ChangeLanguage("RUSSIAN");
         }
         [TestMethod] //CHANGE LANGUAGE TO ENGLISH
         public void LanguageEnglish()
         {
             var Language = new Language(Driver);
-            var englishLanguage = Language.English();
+            Language.ChangeLanguage("ENGLISH");
         }
     }
 }
